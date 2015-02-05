7afad84ffe835f85a6ac1779212ab6aec875dc71
John Calabrese <jcalabrese@pivotal.io>
2015-02-05 12:18:46 -0500

    [#86646270] restore of nfs complete
    we can now run a restore of the nfs archive

--- nfs.go
+++ nfs.go
@@ -1,13 +1,21 @@
 package cfbackup
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
 
 	"github.com/pivotalservices/gtils/command"
 	"github.com/pivotalservices/gtils/osutils"
 )
 
-type remoteOperationsInterface interface {
+const (
+	NFS_DIR_PATH         string = "/var/vcap/store"
+	NFS_ARCHIVE_DIR      string = "shared"
+	NFS_DEFAULT_SSH_USER string = "vcap"
+)
+
+type remoteOpsInterface interface {
 	UploadFile(lfile io.Reader) (err error)
 	Path() string
 }
@@ -23,14 +31,14 @@ func BackupNfs(password, ip string, dest io.Writer) (err error) {
 
 type NFSBackup struct {
 	Caller    command.Executer
-	RemoteOps remoteOperationsInterface
+	RemoteOps remoteOpsInterface
 }
 
 var NfsNewRemoteExecuter func(command.SshConfig) (command.Executer, error) = command.NewRemoteExecutor
 
 func NewNFSBackup(password, ip string) (nfs *NFSBackup, err error) {
 	config := command.SshConfig{
-		Username: "vcap",
+		Username: NFS_DEFAULT_SSH_USER,
 		Password: password,
 		Host:     ip,
 		Port:     22,
@@ -46,19 +54,22 @@ func NewNFSBackup(password, ip string) (nfs *NFSBackup, err error) {
 	return
 }
 
+func (s *NFSBackup) Dump(dest io.Writer) (err error) {
+	err = s.Caller.Execute(dest, s.getDumpCommand())
+	return
+}
+
 func (s *NFSBackup) Import(lfile io.Reader) (err error) {
 	if err = s.RemoteOps.UploadFile(lfile); err == nil {
-		//err = s.restore()
+		err = s.Caller.Execute(ioutil.Discard, s.getRestoreCommand())
 	}
 	return
 }
 
 func (s *NFSBackup) getRestoreCommand() string {
-	return "cd /var/vcap/store && tar zx file.tgz"
+	return fmt.Sprintf("cd %s && tar zx %s", NFS_DIR_PATH, s.RemoteOps.Path())
 }
 
-func (s *NFSBackup) Dump(dest io.Writer) (err error) {
-	command := "cd /var/vcap/store && tar cz shared"
-	err = s.Caller.Execute(dest, command)
-	return
+func (s *NFSBackup) getDumpCommand() string {
+	return fmt.Sprintf("cd %s && tar cz %s", NFS_DIR_PATH, NFS_ARCHIVE_DIR)
 }
